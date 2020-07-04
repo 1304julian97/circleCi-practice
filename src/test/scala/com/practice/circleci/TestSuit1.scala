d67f1344b12b3a832151368ab0add6d904ff928c
Julián Carvajal Montoya <julian@MacBook-Air-de-Julian.local>
2020-07-04 18:41:12 -0500

    Add first test

--- src/test/scala/com/practice/circleci/TestSuit1.scala
+++ src/test/scala/com/practice/circleci/TestSuit1.scala
@@ -1,5 +1,16 @@
 package com.practice.circleci
 
-class TestSuit1 {
+import org.scalatest.{Outcome, fixture}
 
+
+class TestSuit1 extends fixture.FunSuite {
+  override protected def withFixture(test: OneArgTest): Outcome = {
+    test("1","2","3")
+  }
+
+  override type FixtureParam = (String,String,String)
+
+  test("first test"){ f =>
+    assert(true)
+  }
 }

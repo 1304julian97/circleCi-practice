0e05c79f4d81344b73eda115d043b0ba2e318a22
Julián Carvajal Montoya <julian@MacBook-Air-de-Julian.local>
2020-07-05 19:30:13 -0500

    Add 4 suites to learn about fixtures

--- src/test/scala/com/practice/circleci/TestSuit1.scala
+++ src/test/scala/com/practice/circleci/TestSuit1.scala
@@ -5,12 +5,26 @@ import org.scalatest.{Outcome, fixture}
 
 class TestSuit1 extends fixture.FunSuite {
   override protected def withFixture(test: OneArgTest): Outcome = {
-    test("1","2","3")
+    val s1 = "1"
+    val s2 = "2"
+    val s3 = "3"
+    println("Running fixture")
+    test(s1,s2,s3)
   }
 
   override type FixtureParam = (String,String,String)
 
   test("first test"){ f =>
+    println("Running test 1")
+    val (string1,string2,string3) = f
+    assertResult(f._1)(string1)
+    assertResult(f._2)(string2)
+    assertResult(f._3)(string3)
+  }
+
+
+  test("Second test"){f =>
+    println("Running test 2")
     assert(true)
   }
 }
---
+++ src/test/scala/com/practice/circleci/TestSuit4.scala
@@ -0,0 +1,46 @@
+package com.practice.circleci
+
+import org.scalatest.flatspec.AnyFlatSpecLike
+import org.scalatest.matchers.should.Matchers
+
+class TestSuit4 extends AnyFlatSpecLike with Matchers{
+
+  type Fixture = (String, String)
+
+  def testApp(slogan: String = "1")(test: Fixture => Unit)={
+    println("Running fixture")
+    test((slogan, "2"))
+  }
+
+  def testApp2(slogan: String = "1")(test: Fixture => Unit)={
+    println("Running fixture2")
+    test((slogan, "2"))
+  }
+
+  it should "Test1" in testApp(){  f=>
+    println("Running test1")
+    assertResult("1")(f._1)
+    assertResult("2")(f._2)
+
+  }
+
+  it should "Test2" in testApp("3"){ f=>
+    println("Running test2")
+    assertResult("3")(f._1)
+    assertResult("2")(f._2)
+  }
+
+  it should "test3" in testApp2("4"){ f=>
+    println("Running test3*******************************************")
+    assertResult("4")(f._1)
+    assertResult("2")(f._2)
+
+  }
+
+  it should "test4" in {
+    println("Running without fixture")
+    assert(true)
+  }
+
+
+}

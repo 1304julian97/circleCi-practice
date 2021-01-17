49b1029223ca72fb45783e282255c7bb93233aa0
Julián Carvajal <julian@MacBook-Air-de-Julian.local>
2021-01-17 13:21:51 -0500

    Add Parallel suite

--- src/test/scala/com/practice/circleci/TestSuite11.scala
+++ src/test/scala/com/practice/circleci/TestSuite11.scala
@@ -1,5 +1,20 @@
 package com.practice.circleci
 
-class TestSuite11 {
+import org.scalatest.flatspec.AsyncFlatSpecLike
+import org.scalatest.matchers.should.Matchers
 
+import scala.concurrent.Future
+
+class TestSuite11 extends Matchers with AsyncFlatSpecLike{
+
+
+  it should "Future. traverse" in {
+    val list = List(1,2,3,4,5,6,7,7)
+    val f: Int => Future[Int] = i => {
+      Thread.sleep(10000)
+      Future(i+1)
+    }
+    val finalFuture: Future[List[Int]] = Future.traverse(list)(f)
+    finalFuture.map(x=> assertResult(x,"Bad List")(List(2,3,4,5,6,7,8,8)))
+  }
 }
